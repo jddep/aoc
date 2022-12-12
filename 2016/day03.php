efb1b7325c182037dfa55f547588c5163cc64c17
Jesus Diaz <Jesus.Diaz@dealereprocess.com>
2022-12-12 23:33:29 +0000

    Finished 2016 day 3.

---
+++ 2016/day03.php
@@ -0,0 +1,32 @@
+<?php
+$file = file("./data/day03.txt", FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+
+function valid_triangle(int $a, int $b, int $c): bool {
+	$max = max($a, $b, $c);
+	$min = min($a, $b, $c);
+	return $a + $b + $c - $max > $max;
+}
+
+$total = 0;
+foreach ($file as $line) {
+	$line = array_filter(explode(' ', $line));
+	$total += valid_triangle(...$line) ? 1 : 0;
+}
+echo $total;
+
+// part 2
+$one = [];
+$two = [];
+$three = [];
+foreach ($file as $line) {
+	$line = array_filter(explode(' ', $line));
+	print_r($line);
+}
+$nums = array_merge($one, $two, $three);
+sort($nums);
+print_r($nums);
+$total = 0;
+for ($i = 0; $i < count($nums); $i += 3) {
+	$total += valid_triangle($nums[$i], $nums[$i + 1], $nums[$i + 2]) ? 1 : 0;
+}
+echo "\n". $total;
